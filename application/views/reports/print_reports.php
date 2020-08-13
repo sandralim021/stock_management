6d8195612680732ae6941299c92354f570738618
Kaycee Lim <sandralim298@gmail.com>
2020-08-13 19:42:43 +0800

    Print report fix

--- application/views/reports/print_reports.php
+++ application/views/reports/print_reports.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/print/report.css">
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <title>Print Report</title>
 </head>
 <body>
